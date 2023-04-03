527d6c72bfcbc99513ff4802ec72163e9aba3969
H-Alexa <tatineesarkersunom@gmail.com>
2023-04-03 20:31:09 +0600

    __

--- Bot/Booking/booking.py
+++ Bot/Booking/booking.py
@@ -1,16 +1,33 @@
 import os
 import Booking.constants as const
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 
 class Booking(webdriver.Firefox):
-    # def __init__ (self,teardown):
-    def __init__ (self):
+    def __init__ (self,teardown):
+    # def __init__ (self):
 
-        # self.teardown = teardown
+        self.teardown = teardown
         super(Booking, self).__init__()
+        # self.implicitly_wait(15)
+        self.maximize_window()
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # if self.teardown:
+        if self.teardown:
             self.quit()
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    def change_currency(self, currency=None):
+        currency = self.find_element(By.CLASS_NAME, 'e57ffa4eb5')
+        currency.click()
+        selected_currency = self.find_element(By.CLASS_NAME,'cf67405157')
+        selected_currency.click()
+
+    def selectPlaceToGo(self, placeToGo):
+         search_field = self.find_element(By.ID, ':Ra9:')
+         search_field.clear()
+         search_field.send_keys(placeToGo)
--- Bot/main.py
+++ Bot/main.py
@@ -3,9 +3,11 @@ from Booking.booking import Booking
 
 #context manager: __Exit__ teardown 
 # with Booking(teardown=True) as bot:
-with Booking() as bot:
+with Booking(teardown=False) as bot:
     bot.land_first_page()
-    print('Exiting')
+    bot.change_currency()
+    bot.selectPlaceToGo('New York')
+    # print('Exiting')
 
 # inst = Booking()
 # inst.land_first_page()
